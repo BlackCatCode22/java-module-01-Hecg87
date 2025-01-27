1bac1330815bd4f011ce98149e2cc361fd1e6d07
Hecg87 <0428556@MY.SCCCD.EDU>
2025-01-26 20:09:58 -0800

    Add files via upload

---
+++ SimpleGreeting.java
@@ -0,0 +1,13 @@
+// simpleGreeting.java
+//HG 1/26/2025
+//1) HelloWorld Program
+//refactor to rename the default "Main" to something meaningful to you
+
+
+package HelloWorld;
+
+public class SimpleGreeting {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
