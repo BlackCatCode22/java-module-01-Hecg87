857e878f40ee0344eab87db0224ec0d99c214a4d
Hecg87 <0428556@MY.SCCCD.EDU>
2025-01-26 21:35:28 -0800

    Add files via upload

---
+++ VarsAndMath.java
@@ -0,0 +1,17 @@
+public class VarsAndMath {
+    public static void main(String[] args) {
+
+        System.out.println("\n Welcome to Variables and Math!! \n");
+        int a = 10;
+        double b = 5.5;
+        double sum = a + b;
+        System.out.println("\n\n The sum of " + a + " and " + b + " is " + sum);
+        double product = a * b;
+        System.out.println("The Product of " + a + " and " + b + " is: " + product);
+        a = 20;
+        sum = a + b;
+
+        System.out.println("\n\nThe new sum of " + a + " and " + b + " is: " + sum + "\n\n");
+
+    }
+}
